0614b7faa47ff99243bf21cabe62b8e55b122ba5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-09-12 15:31:06 +0200

    Update __manifest__.py

--- maintenance_equipment_operation/__manifest__.py
+++ maintenance_equipment_operation/__manifest__.py
@@ -27,7 +27,6 @@
     'contributor': '',
     'maintainer': 'Vertel AB',
     'repository': 'https://github.com/vertelab/odoo-maintenance',
-    'category': 'Tools',
     'version': '14.0.1.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'license': 'AGPL-3',
