d6a84c083edab2e89b02262b220318f596ef7416
Jakob Krabbe <jakob@syscare.se>
2024-08-13 12:29:29 +0000

    maintenance_property_responsibility: preparing for apps.odoo.com , icons + text

--- maintenance_property_responsibility/__manifest__.py
+++ maintenance_property_responsibility/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Maintenance: Property Glue',
-    'version': '16.0.0.0.0',
+    'version': '0.1',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bu>
     'summary': ' Glue module between odoo_property and maintenance.',
     # Categories can be used to filter modules in modules listing
