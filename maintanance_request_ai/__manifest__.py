8f5263acc326300ad1db0f9c77b72fa106225011
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-10-30 13:15:39 +0100

    T/6250 missing license Update __manifest__.py

--- maintanance_request_ai/__manifest__.py
+++ maintanance_request_ai/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Maintenance Request AI",
     "version": "1.0",
+    'license': 'AGPL-3',
     "depends": ["maintenance", "ai_agent"],
     "data": [
         'data/ai_agent_data.xml',
